6f5dd032519b965a2c3d7f9a790d4714e33f6529
Luiz Henrique <luizhjramos@outlook.com>
2023-05-07 12:50:44 -0300

    test: ensure throw new error when name is empty

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -6,4 +6,10 @@ describe("Customer unir tests", () => {
         new Customer("", "John Doe")
        }).toThrowError("Id is required")
     })
+
+    it("should throw new error when name is empty", () => {
+       expect(() => {
+        new Customer("123", "")
+       }).toThrowError("Name is required")
+    })
 })
