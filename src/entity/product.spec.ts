29741adf6e368b12514fb18d7865ddc0d5ece45e
Luiz Henrique <luizhjramos@outlook.com>
2023-05-10 05:53:48 -0300

    test: ensure throw error when id is empty

---
+++ src/entity/product.spec.ts
@@ -0,0 +1,11 @@
+import { Product } from './product';
+
+
+describe("Order unit tests", () => {
+  it("should throw error when id is empty", () => {
+    expect(() => {
+      const product = new Product("", "Product 1", 100)
+    }).toThrowError("Id is required")
+  });
+
+});
