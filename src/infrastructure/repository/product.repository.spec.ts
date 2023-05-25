1a21921b0978448a45028993e9ce6f3180bdf9b0
Luiz Henrique <luizhjramos@outlook.com>
2023-05-25 08:05:39 -0300

    test: ensure be able create a product

---
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -0,0 +1,38 @@
+import { Sequelize } from "sequelize-typescript";
+import { ProductModel } from "../db/sequelize/model/product.model";
+import { Product } from "../../domain/entity/product";
+import { ProductRepository } from "./product.repository";
+
+describe("Product repository test", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true },
+    });
+
+    sequelize.addModels([ProductModel]);
+    await sequelize.sync();
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  });
+
+  it("should be able create a product", async () => {
+    const productRepository = new ProductRepository();
+    const product = new Product("1", "Product 1", 100);
+
+    await productRepository.create(product);
+    const productModel = await ProductModel.findOne({ where: { id: product.id }});
+
+    expect(productModel.toJSON()).toStrictEqual({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+    });
+  });
+});
